598fc8a089fb8af9a7109f62d0c89ddac2cdab42
irfanMKhan <irfanupdate@yahoo.com>
2025-04-21 09:42:16 -0500

    first commit

---
+++ src/main/java/com/secured/apsd_final/ApsdFinalApplication.java
@@ -0,0 +1,13 @@
+package com.secured.apsd_final;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApsdFinalApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ApsdFinalApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/secured/apsd_final/ApsdFinalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.secured.apsd_final;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApsdFinalApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
